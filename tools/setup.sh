dbf2b3b42886a8abab778ec611b5e66c4ed4084d
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-11 07:08:41 +0900

    Update setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Install n64graphics for texture conversion
+# Create a shortcut so #include <ultra64.h> works
+
+ln -s ../tools/libreultra/include/PR include/PR
 git clone https://github.com/n64decomp/n64graphics.git tools/n64graphics
 make -C tools/n64graphics
 
